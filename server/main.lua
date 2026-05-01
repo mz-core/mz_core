936d0adb24da6193564f0c07ece87ed7b0bd4657
Mazus <corretormateusqueiroz@gmail.com>
2026-04-30 23:01:46 -0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -30,21 +30,28 @@ lib.callback.register('mz_core:server:getSpawnData', function(source)
 
   if not player then return nil end
 
+  local appearance = type(player.metadata) == 'table' and player.metadata.appearance or nil
+  local model = type(appearance) == 'table' and tostring(appearance.model or '') or ''
+  if model ~= 'mp_m_freemode_01' and model ~= 'mp_f_freemode_01' then
+    model = 'mp_m_freemode_01'
+  end
+
   local lastPosition = MZPlayerService.getLastPosition(source)
   if lastPosition then
     return {
       x = lastPosition.x,
       y = lastPosition.y,
       z = lastPosition.z,
-      heading = lastPosition.heading or 0.0
+      heading = lastPosition.heading or 0.0,
+      model = model
     }
   end
 
   return {
-  x = Config.DefaultSpawn.x,
-  y = Config.DefaultSpawn.y,
-  z = Config.DefaultSpawn.z,
-  heading = Config.DefaultSpawn.heading or 0.0,
-  model = 'mp_m_freemode_01'
-}
+    x = Config.DefaultSpawn.x,
+    y = Config.DefaultSpawn.y,
+    z = Config.DefaultSpawn.z,
+    heading = Config.DefaultSpawn.heading or 0.0,
+    model = model
+  }
 end)
